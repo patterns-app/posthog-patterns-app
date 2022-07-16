55139ee6e2f75464ca89954a08f34430d9f0ab31
balajivenkatesh <mjbalajivenkatesh@gmail.com>
2022-07-16 09:08:53 -0700

    Init PostHog app

---
+++ README.md
@@ -0,0 +1,13 @@
+# PostHog Patterns App
+
+Send event data to a webhook endpoint in a [patterns.app](https://patterns.app/) graph.
+
+## Installation
+
+- Create a graph in Patterns and add a webhook node in it. ![Patterns Graph Webhook](patterns_graph_webhook.png)
+- Copy the webhook URL from the sidebar.
+- Install Patterns app from PostHog. Paste the URL in "Patterns Webhook URL" during app configuration.
+
+## We'd love to connect
+
+- [Patterns Beta Access](https://www.patterns.app/beta)
---
+++ index.ts
@@ -0,0 +1,42 @@
+import {
+  PluginInput,
+  Plugin,
+  Meta,
+  RetryError,
+  PluginEvent,
+} from "@posthog/plugin-scaffold";
+import fetch, { Response } from "node-fetch";
+
+interface PatternsPluginInput extends PluginInput {
+  config: {
+    webhookUrl: string;
+  };
+}
+
+// Plugin method that runs on plugin load
+export async function setupPlugin({ config }: Meta<PatternsPluginInput>) {
+  console.log("Loaded Patterns app.");
+}
+
+// Plugin method to export events
+export const exportEvents: Plugin<PatternsPluginInput>["exportEvents"] = async (
+  events: PluginEvent[],
+  { config }: Meta<PatternsPluginInput>
+) => {
+  console.log(
+    `Exporting events to Patterns webhook... ${events.length} events`
+  );
+
+  let response: Response;
+  response = await fetch(config.webhookUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(events),
+  });
+
+  if (response.status != 200) {
+    const data = await response.json();
+    throw new RetryError(`Export events failed: ${JSON.stringify(data)}`);
+  }
+  console.log("Export Success.");
+};
