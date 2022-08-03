3bbb52d07abe2ad9c4d0e2f2ea5294afe4180330
balajivenkatesh <mjbalajivenkatesh@gmail.com>
2022-08-02 18:28:34 -0700

    Update index.test.ts

--- index.test.ts
+++ index.test.ts
@@ -18,17 +18,18 @@ beforeEach(() => {
   fetchMock.resetMocks();
 });
 
-test("exportEvents", async () => {
-  const event = createEvent({ event: "$pageView" });
+test("exportEvents called for events", async () => {
+  const event1 = createEvent({ event: "$pageView" });
+  const event2 = createEvent({ event: "$pageLeave" });
 
   // @ts-ignore
-  await exportEvents([event], getMeta());
+  await exportEvents([event1, event2], getMeta());
 
   expect(fetchMock.mock.calls.length).toEqual(1);
   expect(fetchMock.mock.calls[0][0]).toEqual(getMeta().config.webhookUrl);
   expect(fetchMock.mock.calls[0][1]).toEqual({
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify([event]),
+    body: JSON.stringify([event1, event2]),
   });
 });
