93d20d9f82afb51d565be53e5fd378c26a1219a3
balajivenkatesh <mjbalajivenkatesh@gmail.com>
2022-10-01 12:55:54 +0530

    Add event type filter and test

--- index.test.ts
+++ index.test.ts
@@ -9,10 +9,13 @@ fetchMock.enableMocks();
 
 import { exportEvents } from "./index";
 
+const testWebhookUrl =
+  "https://api-staging.patterns.app/api/app/webhooks/wh1234";
+
 beforeEach(() => {
   resetMeta({
     config: {
-      webhookUrl: "https://api-staging.patterns.app/api/app/webhooks/wh1234",
+      webhookUrl: testWebhookUrl,
     },
   });
   fetchMock.resetMocks();
@@ -31,5 +34,30 @@ test("exportEvents called for events", async () => {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify([event1, event2]),
+  });
+});
+
+test("exportEvents called for only allowed events", async () => {
+  resetMeta({
+    config: {
+      webhookUrl: testWebhookUrl,
+      allowedEventTypes: "$pageView, $autoCapture, $customEvent1",
+    },
+  });
+
+  const event1 = createEvent({ event: "$pageView" });
+  const event2 = createEvent({ event: "$pageLeave" });
+  const event3 = createEvent({ event: "$pageView" });
+  const event4 = createEvent({ event: "$autoCapture" });
+
+  // @ts-ignore
+  await exportEvents([event1, event2, event3, event4], getMeta());
+
+  expect(fetchMock.mock.calls.length).toEqual(1);
+  expect(fetchMock.mock.calls[0][0]).toEqual(getMeta().config.webhookUrl);
+  expect(fetchMock.mock.calls[0][1]).toEqual({
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify([event1, event3, event4]),
   });
 });
